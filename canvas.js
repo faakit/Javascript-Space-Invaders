12a5eefb14c4a450227721455284bb254ea3dfb1
Gustavo Fim Beloti <gustavo.fim.beloti@gmail.com>
2021-03-12 21:31:02 -0300

    Refactor everything

--- canvas.js
+++ canvas.js
@@ -1,35 +1,19 @@
 "use strict";
 
-// --------- GUI
-let board = document.getElementById("space");
-let context = board.getContext("2d");
-let border = "Black";
-let background = "Blue";
-
-// Desenha o quadro
-function drawBoard() {
-    context.fillStyle = background;
-    context.strokeStyle = border;
-    context.fillRect(0, 0, board.width, board.height);
-    context.strokeRect(0, 0, board.width, board.height);
-}
-
-function keyPress(event) {
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
-    const SPACEBAR = 32;
-
-    let keyPressed = event.keyCode;
-    switch (keyPressed) {
-        case RIGHT_KEY:
-            player.move(20);
-            break;
-        case LEFT_KEY:
-            player.move(-20);
-            break;
-        case SPACEBAR:
-            rockets.push(player.shoot());
-            break;
+class Canvas {
+    constructor() {
+        this.board = document.getElementById("space");
+        this.context = this.board.getContext("2d");
+        this.border = "Black";
+        this.background = "Blue";
     }
-}
 
+    // Desenha o quadro
+    draw() {
+        this.context.fillStyle = this.background;
+        this.context.strokeStyle = this.border;
+        this.context.fillRect(0, 0, this.board.width, this.board.height);
+        this.context.strokeRect(0, 0, this.board.width, this.board.height);
+    }
+
+}
